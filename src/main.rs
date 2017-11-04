e84415687f2c7d86cddb41dc001d1cead61355d5
Alex Huddleston <shadow8t4@tamu.edu>
2017-11-04 16:15:23 -0500

    Oops, now it can run.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,43 @@
 extern crate obj;
 extern crate noise;
 
+use std::io::Result;
 use std::path::Path;
+use obj::{Obj, SimplePolygon};
+
+// A function called test that takes in 1 32-bit integer
+// and returns a 32-bit integer.
+/*
+fn test(x: i32) -> i32 {
+    // Yay conditions.
+    if x > 4 {
+        return x;
+    }
+
+    // Normally, we could do this to return.
+    return x + 4;
+    // Where we would need a semicolon because
+    // we are passing the return to the return keyword
+    // rather than just ending the function.
+
+    // This doesn't need a semicolon because it will be
+    // inplicitly returned since it is as the end of the
+    // function definition.
+    x + 4
+}
+*/
 
 fn main() {
-    let obj = obj::Obj::load("data/test.obj").unwrap();
 
-    println!("Postiion: {}", obj.position);
+    let path = Path::new("data/teapot.obj");
+    let maybe_obj: Result<Obj<SimplePolygon>> = Obj::load(&path);
+
+    if let Ok(obj) = maybe_obj {
+        println!("Postiion: {:?}", obj.position);
+    }
+    /*
+    else if Err(error) = maybe_obj {
+
+    }
+    */
 }
